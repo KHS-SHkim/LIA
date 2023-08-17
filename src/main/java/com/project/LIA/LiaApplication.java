28a2d7c7e3ae6d3f9f3210547360972e2a3db47f
IamHongSeok <hongseok890507@gmail.com>
2023-08-17 20:55:44 +0900

    20230817 : # 김홍석 BASE 프로젝트 생성

---
+++ src/main/java/com/project/LIA/LiaApplication.java
@@ -0,0 +1,13 @@
+package com.project.LIA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/LIA/LiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.LIA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
