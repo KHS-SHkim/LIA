392df2db6f18f8afc31b85e277ec1553771c2706
leegayeon <99721126+dlrkdus@users.noreply.github.com>
2023-08-22 19:23:10 +0900

    2
    도메인+regdate 클래스

---
+++ src/main/java/com/project/LIA/domain/BaseEntity.java
@@ -0,0 +1,30 @@
+package com.project.LIA.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.project.LIA.listener.Auditable;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Data
+@MappedSuperclass
+@EntityListeners(value = AuditingEntityListener.class)
+public class BaseEntity implements Auditable {
+
+    @CreatedDate   // Entity 생성시점의 시간 입력
+    // java.time.*  객체 변환을 위한 annotation
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    @JsonProperty("regdate")
+    private LocalDateTime regDate;
+}
--- src/main/java/com/project/LIA/domain/BookDomain.java
+++ src/main/java/com/project/LIA/domain/BookDomain.java
@@ -1,13 +1,13 @@
 package com.project.LIA.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -17,7 +17,46 @@ import lombok.NoArgsConstructor;
 public class BookDomain {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private int id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String cate; // 카테고리
+
+    @Column(nullable = false)
+    private int price;
+
+    @Column(nullable = false)
+    private String rental_spot; // 대여지역
+
+    @Column(nullable = false)
+    private String book_detail; // content
+
+    @Column(nullable = false)
+    private String rental_stat; // 대여상태
+
+    @Column(nullable = false)
+    private int rental_date; // 대여기간
+
+    @ManyToOne
+    @ToString.Exclude
+    private UserDomain user;
+
+    // 첨부파일
+    // Book:BookImg = 1:N
+    //  cascade = CascadeType.ALL  : 삭제등의 동작 발생시 child 도 함께 삭제
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "book_id")
+    @ToString.Exclude
+    @Builder.Default
+    private List<BookImgDomain> fileList = new ArrayList<>();
+
+    public void addFiles(BookImgDomain... files){
+        Collections.addAll(fileList, files);
+    }
+
 
 
 
