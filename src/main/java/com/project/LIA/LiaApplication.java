b8ad10553fd10a60c15fe7db73cd7a6bd4068ebd
seunggipark <fnrrlfnrrl3@naver.com>
2023-08-22 21:48:25 +0900

    # static/image 디렉토리 추가 EmailService/ EmailServiceImpl 회원가입 js 추가

--- src/main/java/com/project/LIA/LiaApplication.java
+++ src/main/java/com/project/LIA/LiaApplication.java
@@ -1,9 +1,12 @@
 package com.project.LIA;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class LiaApplication {
 
 	public static void main(String[] args) {
