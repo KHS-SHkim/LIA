5e97a166ff43293492866c1b131db8e6d5356cd0
IamHongSeok <hongseok890507@gmail.com>
2023-08-30 21:47:44 +0900

    20230830: #김홍석

--- src/main/java/com/project/LIA/controller/NoteController.java
+++ src/main/java/com/project/LIA/controller/NoteController.java
@@ -9,9 +9,7 @@ import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -56,6 +54,11 @@ public class NoteController {
 
         }
         return "/note/detail";
+    }
+
+    @PostMapping("/write/{user_id}/{receiver_id}")
+    public String noteWrite(@RequestParam("contents") String contents, @PathVariable Long user_id, @PathVariable Long receiver_id){
+        return "";
     }
 
 }
