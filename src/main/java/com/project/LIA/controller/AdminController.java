2ab0be89fbf2755d814a88580be751389e700e47
seunggipark <fnrrlfnrrl3@naver.com>
2023-09-03 02:46:26 +0900

    2023 09 03 박승기 관리자 페이지 작업

--- src/main/java/com/project/LIA/controller/AdminController.java
+++ src/main/java/com/project/LIA/controller/AdminController.java
@@ -1,18 +1,26 @@
 package com.project.LIA.controller;
 
 import com.project.LIA.domain.UserDomain;
+import com.project.LIA.service.AdminService;
 import com.project.LIA.service.UserService;
 import com.project.LIA.util.U;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+    @Autowired
+    AdminService adminService;
+
     @Autowired
     UserService userService;
 
@@ -33,6 +41,15 @@ public class AdminController {
         }
     }
 
+    @GetMapping("/userList")
+    public void userList(Model model){
+        List<UserDomain> userList = adminService.userList();
+        if(userList != null){
+            model.addAttribute("userList",userList);
+        } else{
+            model.addAttribute("userList",null);
+        }
+    }
 
 
 
