ab278de42ddbd74cba116cd433046bf6324d05d6
seunggipark <fnrrlfnrrl3@naver.com>
2023-09-05 20:54:32 +0900

    2023-09-05 박승기 시큐리티 접근권한 수정 (qna)부분

--- src/main/java/com/project/LIA/config/SecurityConfig.java
+++ src/main/java/com/project/LIA/config/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         // URL 과 접근관한 세팅(들)
                         // ↓ /board/detail/** URL로 들어오는 요청은 '인증'만 필요.
-//                        .requestMatchers("/user/mypage/**").authenticated()
+                        .requestMatchers("/qna/write/**").authenticated()
                         // ↓ "/board/write/**", "/board/update/**", "/board/delete/**" URL로 들어오는 요청은 '인증' 뿐 아니라 ROLE_MEMBER 나 ROLE_ADMIN 권한을 갖고 있어야 한다. ('인가')
                         .requestMatchers("/user/myPage/**").hasAnyRole("MEMBER", "ADMIN")
                         .requestMatchers("/admin/**").hasAnyRole("ADMIN")
